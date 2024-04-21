553f53d318aa90cd1c3c3646d6faddc4b7495e27
slicequeue <slicequeue@gmail.com>
2024-04-21 16:51:08 +0900

    set: 의존성 추가, 설정 관련 변경

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 console.log('__dirname', __dirname);
-dotenv.config({ path: path.join(__dirname, `.${process.env.NODE_ENV}.env`) });
+dotenv.config({ path: path.join(__dirname, `${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}.env`) });
 
 const packageJson = require('../../package.json');
 
@@ -29,14 +29,9 @@ const config = {
     refreshTokenExpiration: cast('ACCESS_TOKEN_EXPIRATION', 'string', '60d')
     */
   },
-  // db: {
-  //   host: cast('DB_HOST', 'string', 'localhost'),
-  //   port: cast('DB_PORT', 'number', 27017),
-  //   username: cast('DB_ID', 'string'),
-  //   password: cast('DB_PW', 'string'),
-  //   database: cast('DB_DATABASE', 'string'),
-  //   poolSize: cast('DB_POOLSIZE', 'number', 5),
-  // }
+  govApi: {
+    key: cast('GOV_API_KEY', 'string'),
+  }
 };
 
 module.exports = config;
